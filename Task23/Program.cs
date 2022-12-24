bdcabd404565baca82f02fd2ccd1fb788c72ddac
ZAndreyV <119517277+ZAndreyV@users.noreply.github.com>
2022-12-24 22:33:10 +0700

    added Task23

---
+++ Task23/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Задача 23
+Напишите программу, которая принимает на вход число (N)
+ и выдаёт таблицу кубов чисел от 1 до N.
+5 ->
+1 | 1
+2 | 8
+3 | 27
+4 | 64
+5 | 125 */
+
+Console.WriteLine("Enter number N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+CubeTable(number);
+
+void CubeTable(int num)
+{
+    for (int i = 1; i < num; i++)
+    {
+        Console.WriteLine($"{i,7}  {Math.Pow(i, 3),7}");
+    }
+   
+}
+
+
