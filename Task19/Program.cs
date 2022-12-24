73dd167dd4859f67c15d34ed5e8fab22362633f2
ZAndreyV <119517277+ZAndreyV@users.noreply.github.com>
2022-12-25 01:24:48 +0700

    added .gitignore

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -21,13 +21,7 @@ while (number > 0)
 return num == pal;
 }
 
-    
 
-// int pal = 0, 
-// num = number;
-// while (number > 0) 
-// {
-//     pal = pal * 10 + number % 10; number /= 10;
-// }
-// Console.WriteLine(num == pal ? "палиндром" : "не палиндром");
-    
+
+
+
